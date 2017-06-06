d41a865b9b5c5bf4cc81921f6f7a8d4009b684fb
xiemeilong <xiemeilong.ok@gmail.com>
2017-06-06 20:30:37 +0800

    Update and rename pac.js to my.pac

--- pac.js
+++
@@ -1,8 +0,0 @@
-function FindProxyForURL(url, host) {
-	if (shExpMatch(host, "*.google.com,twitter.com")||shExpMatch(host, "*.twitter.com")||shExpMatch(host, "*.twimg.com")||shExpMatch(host, "t.co"))
-	{
-			return "PROXY http://localhost:8788; DIRECT";
-	}
-
-	return "DIRECT";
-}
