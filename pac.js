ba8cf4b0dd5bfac27f98d7ff8cb4bbce5676a48f
xiemeilong <xiemeilong.ok@gmail.com>
2017-06-06 20:26:41 +0800

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,7 +1,7 @@
 function FindProxyForURL(url, host) {
 	if (shExpMatch(host, "*.google.com,twitter.com")||shExpMatch(host, "*.twitter.com")||shExpMatch(host, "*.twimg.com")||shExpMatch(host, "t.co"))
 	{
-			return "PROXY localhost:8787; DIRECT";
+			return "PROXY http://localhost:8788; DIRECT";
 	}
 
 	return "DIRECT";
